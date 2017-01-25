7a4d63e6490c9054d90cdc969acc819b07dd4d19
Malith Munasinghe <mpmunasinghe@users.noreply.github.com>
2017-01-25 18:38:59 +0530

    Update stop-all.sh

--- distribution/src/scripts/stop-all.sh
+++ distribution/src/scripts/stop-all.sh
@@ -19,13 +19,13 @@
 PRG="$0"
 
 while [ -h "$PRG" ]; do
-ls=`ls -ld "$PRG"`
-link=`expr "$ls" : '.*-> \(.*\)$'`
-if expr "$link" : '.*/.*' > /dev/null; then
-PRG="$link"
-else
-PRG=`dirname "$PRG"`/"$link"
-fi
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '.*/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
 done
 
 # Get standard environment variables
