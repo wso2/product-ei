4c887768afa91d6031ea10704583ebd9388a7467
Nandika Jayawardana <jayawark@gmail.com>
2017-03-31 22:17:46 +0530

    Update ciphertool.bat

--- distribution/src/scripts/ciphertool.bat
+++ distribution/src/scripts/ciphertool.bat
@@ -74,7 +74,7 @@ rem set the classes
 setlocal EnableDelayedExpansion
 rem loop through the libs and add them to the class path
 cd "%CARBON_HOME%"
-call ant -buildfile "%CARBON_HOME%\bin\build.xml" -q
+
 set CARBON_CLASSPATH=.\conf
 FOR %%c in ("%CARBON_HOME%\wso2\lib\*.jar") DO set CARBON_CLASSPATH=!CARBON_CLASSPATH!;".\wso2\lib\%%~nc%%~xc"
 FOR %%C in ("%CARBON_HOME%\repository\lib\*.jar") DO set CARBON_CLASSPATH=!CARBON_CLASSPATH!;".\repository\lib\%%~nC%%~xC"
