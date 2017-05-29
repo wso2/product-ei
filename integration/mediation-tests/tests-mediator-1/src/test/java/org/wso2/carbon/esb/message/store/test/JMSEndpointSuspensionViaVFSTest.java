17380bb961b3f6769e798e44cb81f3e6db42a357
Himasha Guruge <himashag@wso2.com>
2017-05-29 12:14:55 +0530

    Add assertion message to find intermittent test failure

--- integration/mediation-tests/tests-mediator-1/src/test/java/org/wso2/carbon/esb/message/store/test/JMSEndpointSuspensionViaVFSTest.java
+++ integration/mediation-tests/tests-mediator-1/src/test/java/org/wso2/carbon/esb/message/store/test/JMSEndpointSuspensionViaVFSTest.java
@@ -135,7 +135,7 @@ public class JMSEndpointSuspensionViaVFSTest extends ESBIntegrationTest {
 
         sendFile(outfile, afile, bfile);
 
-        Assert.assertTrue(interceptor.getPayload().contains("Endpoint Down!"));
+        Assert.assertTrue(interceptor.getPayload().contains("Endpoint Down!"),"payload received:" + interceptor.getPayload());
 
         deleteProxyService("VFSJMSProxy1");
     }
