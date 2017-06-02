aa9783309688955c8c2d80022d86df627cd9e90f
pumudu <pumudu@wso2.com>
2017-06-02 10:22:43 +0530

    Add testng to product-ei as integration test framework.

---
+++ integration/test-mb/test-amqp/src/test/java/org/wso2/ei/mb/test/amqp/QueueTestCase.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2005-2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.ei.mb.test.amqp;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * Test cases for queue related scenarios.
+ */
+public class QueueTestCase {
+
+    protected Log log = LogFactory.getLog(QueueTestCase.class);
+
+
+    @BeforeClass
+    public void beforeClass() {
+        
+    }
+    
+    @BeforeTest
+    public void beforeTest() {
+        
+    }
+
+    @Test
+    public void performSingleQueueSendReceiveTestCase() {
+        
+    }
+
+    @AfterClass
+    public void afterClass() {
+        
+    }
+
+    @AfterTest
+    public void afterTest() {
+        
+    }
+}
