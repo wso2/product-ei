6d5b631bfed410d1f9ea172cb482ba4910c71ccb
Nuwan Wimalasekara <nuwanw@wso2.com>
2017-06-07 17:12:12 +0530

    improve conditional routing test cases

--- integration/mediation-tests/tests-mediator-1/src/test/java/org/wso2/carbon/esb/mediator/test/conditionalRouter/ConditionalRouterIntegrationTest.java
+++ integration/mediation-tests/tests-mediator-1/src/test/java/org/wso2/carbon/esb/mediator/test/conditionalRouter/ConditionalRouterIntegrationTest.java
@@ -39,20 +39,17 @@ import org.wso2.esb.integration.common.utils.clients.stockquoteclient.StockQuote
 import java.io.File;
 import java.net.URL;
 
-
 public class ConditionalRouterIntegrationTest extends ESBIntegrationTest {
 
     private String toUrl = null;
     private String mainSeqUrl;
     private SequenceAdminServiceClient sequenceAdminServiceClient;
 
-
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
         super.init();
         mainSeqUrl = getMainSequenceURL();
-        sequenceAdminServiceClient = new SequenceAdminServiceClient(contextUrls.getBackEndUrl(),
-                                                                    getSessionCookie());
+        sequenceAdminServiceClient = new SequenceAdminServiceClient(contextUrls.getBackEndUrl(), getSessionCookie());
         toUrl = getBackEndServiceUrl(ESBTestConstant.SIMPLE_STOCK_QUOTE_SERVICE);
 
     }
@@ -64,12 +61,15 @@ public class ConditionalRouterIntegrationTest extends ESBIntegrationTest {
      * @throws Exception
      */
    /* https://wso2.org/jira/browse/STRATOS-2239*/
-    @Test(groups = {"wso2.esb"})
+    @Test(groups = { "wso2.esb" })
     public void conditionalRouterMediatorWithContinueAfterTrueTest() throws Exception {
+        esbUtils.isProxyServiceExist(contextUrls.getBackEndUrl(), sessionCookie,
+                "ConRoutingWithContinueAfterTrueProxy");
+        esbUtils.isSequenceExist(contextUrls.getBackEndUrl(), sessionCookie, "cnd_seq4");
 
-        loadESBConfigurationFromClasspath("/artifacts/ESB/synapseconfig/filters/conditional_router/synapse1.xml");
-
-        OMElement response = axis2Client.sendSimpleStockQuoteRequest(mainSeqUrl, toUrl, "WSO2");
+        OMElement response = axis2Client
+                .sendSimpleStockQuoteRequest(getProxyServiceURLHttp("ConRoutingWithContinueAfterTrueProxy"), toUrl,
+                        "WSO2");
 
         Assert.assertTrue(response.toString().contains("GetQuoteResponse"), "GetQuoteResponse not found in response");
         Assert.assertTrue(response.toString().contains("WSO2 Company"), "GetQuoteResponse not found in response");
@@ -83,19 +83,24 @@ public class ConditionalRouterIntegrationTest extends ESBIntegrationTest {
      * @throws Exception
      */
      /* https://wso2.org/jira/browse/STRATOS-2239*/
-    @Test(groups = {"wso2.esb"})
+    @Test(groups = { "wso2.esb" })
     public void conditionalRouterMediatorWithContinueAfterFalseTest() throws Exception {
+        esbUtils.isProxyServiceExist(contextUrls.getBackEndUrl(), sessionCookie,
+                "ConRoutingWithContinueAfterFalseProxy");
+        esbUtils.isSequenceExist(contextUrls.getBackEndUrl(), sessionCookie, "cnd_seq5");
 
-        loadESBConfigurationFromClasspath("/artifacts/ESB/synapseconfig/filters/conditional_router/synapse2.xml");
-
-        OMElement response = axis2Client.sendSimpleStockQuoteRequest(mainSeqUrl, toUrl, "WSO2");
+        OMElement response = axis2Client
+                .sendSimpleStockQuoteRequest(getProxyServiceURLHttp("ConRoutingWithContinueAfterFalseProxy"), toUrl,
+                        "WSO2");
 
         Assert.assertTrue(response.toString().contains("GetQuoteResponse"));
         Assert.assertTrue(response.toString().contains("WSO2 Company"));
 
         OMElement response2 = null;
         try {
-            response2 = axis2Client.sendSimpleStockQuoteRequest(mainSeqUrl, null, "WSO2");
+            response2 = axis2Client
+                    .sendSimpleStockQuoteRequest(getProxyServiceURLHttp("ConRoutingWithContinueAfterFalseProxy"), null,
+                            "WSO2");
             Assert.fail("This Request should throw AxisFault");
         } catch (AxisFault e) {
 
@@ -112,8 +117,8 @@ public class ConditionalRouterIntegrationTest extends ESBIntegrationTest {
      *
      * @throws Exception
      */
-    @SetEnvironment(executionEnvironments = {ExecutionEnvironment.PLATFORM,ExecutionEnvironment.PLATFORM})
-    @Test(groups = {"wso2.esb"})
+    @SetEnvironment(executionEnvironments = { ExecutionEnvironment.PLATFORM, ExecutionEnvironment.PLATFORM })
+    @Test(groups = { "wso2.esb" })
     public void conditionalRouterMediatorWithMultiRoutesTest() throws Exception {
         loadSampleESBConfiguration(157);
         StockQuoteClient client1 = new StockQuoteClient();
@@ -127,17 +132,20 @@ public class ConditionalRouterIntegrationTest extends ESBIntegrationTest {
         client3.addHttpHeader("my_custom_header2", "bar");
         client3.addHttpHeader("my_custom_header3", "foo");
 
-        OMElement response1 = client1.sendSimpleStockQuoteRequest(getProxyServiceURLHttp("StockQuoteProxy"), null, "WSO2");
+        OMElement response1 = client1
+                .sendSimpleStockQuoteRequest(getProxyServiceURLHttp("StockQuoteProxy"), null, "WSO2");
 
         Assert.assertTrue(response1.toString().contains("GetQuoteResponse"));
         Assert.assertTrue(response1.toString().contains("WSO2 Company"));
 
-        OMElement response2 = client2.sendSimpleStockQuoteRequest(getProxyServiceURLHttp("StockQuoteProxy"), null, "WSO2");
+        OMElement response2 = client2
+                .sendSimpleStockQuoteRequest(getProxyServiceURLHttp("StockQuoteProxy"), null, "WSO2");
 
         Assert.assertTrue(response2.toString().contains("GetQuoteResponse"));
         Assert.assertTrue(response2.toString().contains("WSO2 Company"));
 
-        OMElement response3 = client3.sendSimpleStockQuoteRequest(getProxyServiceURLHttp("StockQuoteProxy") + "?qparam1=qpv_foo&qparam2=qpv_foo2", null, "WSO2");
+        OMElement response3 = client3.sendSimpleStockQuoteRequest(
+                getProxyServiceURLHttp("StockQuoteProxy") + "?qparam1=qpv_foo&qparam2=qpv_foo2", null, "WSO2");
 
         Assert.assertTrue(response3.toString().contains("GetQuoteResponse"));
         Assert.assertTrue(response3.toString().contains("WSO2 Company"));
@@ -150,10 +158,12 @@ public class ConditionalRouterIntegrationTest extends ESBIntegrationTest {
      *
      * @throws Exception
      */
-    @Test(groups = {"wso2.esb"})
+    @Test(groups = { "wso2.esb" })
     public void conditionalRouterMediatorWithBreakRouteTrueTest() throws Exception {
 
         loadESBConfigurationFromClasspath("/artifacts/ESB/synapseconfig/filters/conditional_router/synapse3.xml");
+        esbUtils.isSequenceExist(contextUrls.getBackEndUrl(), sessionCookie, "cnd1_seq");
+        esbUtils.isSequenceExist(contextUrls.getBackEndUrl(), sessionCookie, "cnd2_seq");
         StockQuoteClient client = new StockQuoteClient();
 
         client.addHttpHeader("foo", "bar");
@@ -176,15 +186,18 @@ public class ConditionalRouterIntegrationTest extends ESBIntegrationTest {
      *
      * @throws Exception
      */
-    @Test(groups = {"wso2.esb"}, enabled = false)
+    @Test(groups = { "wso2.esb" },
+          enabled = false)
     public void conditionalRouterMediatorWithBreakRouteFalseTest() throws Exception {
+        esbUtils.isProxyServiceExist(contextUrls.getBackEndUrl(), sessionCookie, "cnd1_seq");
+        esbUtils.isProxyServiceExist(contextUrls.getBackEndUrl(), sessionCookie, "cnd2_seq");
         loadESBConfigurationFromClasspath("/artifacts/ESB/synapseconfig/filters/conditional_router/synapse3.xml");
         StockQuoteClient client = new StockQuoteClient();
 
         client.addHttpHeader("foo", "bar");
 
-        OMElement response = client.sendSimpleStockQuoteRequest(getProxyServiceURLHttp("StockQuoteProxy2"), null, "WSO2");
-
+        OMElement response = client
+                .sendSimpleStockQuoteRequest(getProxyServiceURLHttp("StockQuoteProxy2"), null, "WSO2");
 
         Assert.assertTrue(response.toString().contains("GetQuoteResponse"));
         Assert.assertTrue(response.toString().contains("WSO2 Company"));
@@ -197,24 +210,22 @@ public class ConditionalRouterIntegrationTest extends ESBIntegrationTest {
      *
      * @throws Exception
      */
-    @Test(groups = {"wso2.esb"})
+    @Test(groups = { "wso2.esb" })
     public void conditionalRouterMediatorWithDynamicSequenceTest() throws Exception {
-
 
         ArtifactReaderUtil artifactReaderUtil = new ArtifactReaderUtil();
 
         // Get an OMElement from xml
-        OMElement omElement = artifactReaderUtil.getOMElement(getESBResourceLocation() + File.separator
-                                                              + "synapseconfig" + File.separator
-                                                              + "filters" + File.separator
-                                                              + "conditional_router" + File.separator + "dynamic_seq1.xml");
-
+        OMElement omElement = artifactReaderUtil.getOMElement(
+                getESBResourceLocation() + File.separator + "synapseconfig" + File.separator + "filters"
+                        + File.separator + "conditional_router" + File.separator + "dynamic_seq1.xml");
 
         // Add dynamic sequence to WSO2registry    config/filters/dynamic_seq1
         if (ArrayUtils.contains(sequenceAdminServiceClient.getDynamicSequences(), "conf:/filters/dynamic_seq1")) {
             sequenceAdminServiceClient.deleteDynamicSequence("conf:/filters/dynamic_seq1");
             for (int i = 0; i < 5; i++) {
-                if (!ArrayUtils.contains(sequenceAdminServiceClient.getDynamicSequences(), "conf:/filters/dynamic_seq1")) {
+                if (!ArrayUtils
+                        .contains(sequenceAdminServiceClient.getDynamicSequences(), "conf:/filters/dynamic_seq1")) {
                     break;
                 }
                 Thread.sleep(500);
@@ -240,14 +251,13 @@ public class ConditionalRouterIntegrationTest extends ESBIntegrationTest {
      *
      * @throws Exception
      */
-    @Test(groups = {"wso2.esb"})
+    @Test(groups = { "wso2.esb" })
     public void conditionalRouterMediatorWithManyRoutesTest() throws Exception {
         loadESBConfigurationFromClasspath("/artifacts/ESB/synapseconfig/filters/conditional_router/synapse5.xml");
         StockQuoteClient client = new StockQuoteClient();
         // Note: toUrl is set to null -
-        OMElement response = client.sendSimpleStockQuoteRequest(getProxyServiceURLHttp("conditionalRouterWithManyRoutesProxy")
-                                                                + "?qparam1=qpv_foo", null, "WSO2");
-
+        OMElement response = client.sendSimpleStockQuoteRequest(
+                getProxyServiceURLHttp("conditionalRouterWithManyRoutesProxy") + "?qparam1=qpv_foo", null, "WSO2");
 
         Assert.assertTrue(response.toString().contains("GetQuoteResponse"), "Response message mismatched");
         Assert.assertTrue(response.toString().contains("WSO2 Company"), "Requested Symbol not found in response");
@@ -261,15 +271,17 @@ public class ConditionalRouterIntegrationTest extends ESBIntegrationTest {
      * @throws Exception
      */
     //disabled since it is negative
-    @Test(groups = {"wso2.esb"}, enabled = false)
+    @Test(groups = { "wso2.esb" },
+          enabled = false)
     public void conditionalRouterMediatorWithEqualUrlTest() throws Exception {
         loadESBConfigurationFromClasspath("/artifacts/ESB/synapseconfig/filters/conditional_router/synapse6.xml");
         StockQuoteClient client = new StockQuoteClient();
 
         OMElement response = null;
         try {
-            response = client.sendSimpleStockQuoteRequest(getProxyServiceURLHttp("ServiceProxy1")
-                                                          + "?qparam1=qpv_foo", null, "WSO2");
+            response = client
+                    .sendSimpleStockQuoteRequest(getProxyServiceURLHttp("ServiceProxy1") + "?qparam1=qpv_foo", null,
+                            "WSO2");
             Assert.fail("This Request Should throw AxisFault");
         } catch (AxisFault e) {
         }
@@ -285,7 +297,8 @@ public class ConditionalRouterIntegrationTest extends ESBIntegrationTest {
      * @throws Exception
      */
     //disabled since it is negative
-    @Test(groups = {"wso2.esb"}, enabled = false)
+    @Test(groups = { "wso2.esb" },
+          enabled = false)
     public void conditionalRouterMediatorWithEqualHeaderTest() throws Exception {
         loadESBConfigurationFromClasspath("/artifacts/ESB/synapseconfig/filters/conditional_router/synapse6.xml");
         StockQuoteClient client = new StockQuoteClient();
@@ -294,8 +307,9 @@ public class ConditionalRouterIntegrationTest extends ESBIntegrationTest {
 
         OMElement response = null;
         try {
-            response = client.sendSimpleStockQuoteRequest(getProxyServiceURLHttp("ServiceProxy2")
-                                                          + "?qparam1=qpv_foo", null, "WSO2");
+            response = client
+                    .sendSimpleStockQuoteRequest(getProxyServiceURLHttp("ServiceProxy2") + "?qparam1=qpv_foo", null,
+                            "WSO2");
             Assert.fail("This Request Should throw AxisFault");
         } catch (AxisFault e) {
         }
