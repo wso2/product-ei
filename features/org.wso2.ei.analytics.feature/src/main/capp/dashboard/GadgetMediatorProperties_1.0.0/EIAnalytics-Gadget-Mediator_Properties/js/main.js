2ef283c87e1bfac94a853d733332136b2eb73c58
Tharik Kanaka <tharik.kanaka@gmail.com>
2017-07-06 12:04:40 +0530

    improve message property gadget and date range selection (#708)
    * improve message property gadget code mirror payload formatting
    
    * fix rounding date range selections to last minute by avoiding seconds.
    
    * refactor js code of message properties gadget

--- features/org.wso2.ei.analytics.feature/src/main/capp/dashboard/GadgetMediatorProperties_1.0.0/EIAnalytics-Gadget-Mediator_Properties/js/main.js
+++ features/org.wso2.ei.analytics.feature/src/main/capp/dashboard/GadgetMediatorProperties_1.0.0/EIAnalytics-Gadget-Mediator_Properties/js/main.js
@@ -85,6 +85,16 @@ function onError(msg) {
 
 function drawMergeView(placeholder, before, after) {
     var view = document.getElementById(placeholder);
+    if(isJSON(before)) {
+        before = JSON.stringify(JSON.parse(before), null, "\t");   
+    } else {
+        before = formatXML(before);
+    }
+    if(isJSON(after)) {
+        after = JSON.stringify(JSON.parse(after), null, "\t");  
+    } else {
+        after = formatXML(after);
+    }
     view.innerHTML = "";
       var dv = CodeMirror.MergeView(view, {
         value: before,
@@ -95,3 +105,63 @@ function drawMergeView(placeholder, before, after) {
         connect: "connect"
     });
 }
+
+function isJSON(str) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+
+function formatXML(xml) {
+    var reg = /(>)(<)(\/*)/g;
+    var wsexp = / *(.*) +\n/g;
+    var contexp = /(<.+>)(.+\n)/g;
+    xml = xml.replace(reg, '$1\n$2$3').replace(wsexp, '$1\n').replace(contexp, '$1\n$2');
+    var pad = 0;
+    var formatted = '';
+    var lines = xml.split('\n');
+    var indent = 0;
+    var lastType = 'other';
+    var transitions = {
+        'single->single'    : 0,
+        'single->closing'   : -1,
+        'single->opening'   : 0,
+        'single->other'     : 0,
+        'closing->single'   : 0,
+        'closing->closing'  : -1,
+        'closing->opening'  : 0,
+        'closing->other'    : 0,
+        'opening->single'   : 1,
+        'opening->closing'  : 0, 
+        'opening->opening'  : 1,
+        'opening->other'    : 1,
+        'other->single'     : 0,
+        'other->closing'    : -1,
+        'other->opening'    : 0,
+        'other->other'      : 0
+    };
+
+    for (var i=0; i < lines.length; i++) {
+        var ln = lines[i];
+        var single = Boolean(ln.match(/<.+\/>/));
+        var closing = Boolean(ln.match(/<\/.+>/));
+        var opening = Boolean(ln.match(/<[^!].*>/));
+        var type = single ? 'single' : closing ? 'closing' : opening ? 'opening' : 'other';
+        var fromTo = lastType + '->' + type;
+        lastType = type;
+        var padding = '';
+
+        indent += transitions[fromTo];
+        for (var j = 0; j < indent; j++) {
+            padding += '    ';
+        }
+
+        formatted += padding + ln + '\n';
+    }
+
+    return formatted;
+};
