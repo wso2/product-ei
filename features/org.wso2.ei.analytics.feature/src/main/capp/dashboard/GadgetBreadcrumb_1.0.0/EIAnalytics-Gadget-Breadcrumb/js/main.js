78b8c9b5d858f0a2d1579003e206856ef2745b11
Imesh Chandrasiri <dimal.chandrasiri@gmail.com>
2017-07-06 11:45:19 +0530

    Adding breadcrumb back

--- features/org.wso2.ei.analytics.feature/src/main/capp/dashboard/GadgetBreadcrumb_1.0.0/EIAnalytics-Gadget-Breadcrumb/js/main.js
+++ features/org.wso2.ei.analytics.feature/src/main/capp/dashboard/GadgetBreadcrumb_1.0.0/EIAnalytics-Gadget-Breadcrumb/js/main.js
@@ -7,14 +7,19 @@ var currentLocation;
 
 $(function() {
     
-    $("#homeLink").attr("href", baseUrl);
+    if('message' === pageName){
+        $('.breadcrumb li:first-child()').remove();
+        $('.breadcrumb').append('<li><a class="back" href=""><i class="icon fw fw-redo fw-flip-horizontal"></i> Back</a></li>')
+    }else{
+        $("#homeLink").attr("href", baseUrl);
+    }
+
     currentLocation = pageName;
     if (currentLocation != TYPE_LANDING) {
         appendTrail(lastSegment + pageName,currentLocation);
         if (qs[PARAM_ID] != null) {
             appendTrail(lastSegment + pageName + "?" + PARAM_ID + "=" + qs[PARAM_ID],qs[PARAM_ID]);
         }
-
     }
     
     function appendTrail(url,text) {
@@ -23,7 +28,7 @@ $(function() {
         var a = $('<a/>');
         
         if(['message','mediator'].indexOf(text) > -1){
-            li.addClass("text-muted truncate");
+            li.addClass("text-muted truncate hidden");
             li.append(text);
         }
         else{
@@ -33,11 +38,20 @@ $(function() {
             li.append(a);
         }
         ol.append(li);
+
+        if('message' === pageName){
+            $('.breadcrumb li.hidden').next().addClass('hide-slash');
+        }
     }
-    
+
     $(".breadcrumb").on('click', 'a', function(e) {
         e.preventDefault();
-        parent.window.location = $(this).attr('href');
+        if($(this).hasClass('back')){
+            parent.window.history.back();
+        }else{
+            parent.window.location = $(this).attr('href');
+        }
+        
     });
 
 });
