cf5b70b21fe1e8cce4691136adacfe202f19c5a6
Asanka Abeyweera <asankaab@wso2.com>
2017-09-27 17:26:04 +0530

    Fix JMSInboundTransactionTestCase
    This will test the commit and rollback capabilities with the jms
    inbound transport

--- integration/mediation-tests/tests-transport/src/test/java/org/wso2/carbon/esb/jms/inbound/transport/test/JMSInboundTransactionTestCase.java
+++ integration/mediation-tests/tests-transport/src/test/java/org/wso2/carbon/esb/jms/inbound/transport/test/JMSInboundTransactionTestCase.java
@@ -26,121 +26,126 @@ import org.wso2.carbon.automation.extensions.servers.jmsserver.client.JMSQueueMe
 import org.wso2.carbon.automation.extensions.servers.jmsserver.client.JMSQueueMessageProducer;
 import org.wso2.carbon.automation.extensions.servers.jmsserver.controller.config.JMSBrokerConfigurationProvider;
 import org.wso2.carbon.integration.common.admin.client.LogViewerClient;
-import org.wso2.carbon.logging.view.stub.types.carbon.LogEvent;
 import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
+import org.wso2.esb.integration.common.utils.Utils;
 import org.wso2.esb.integration.common.utils.servers.ActiveMQServer;
 
-import javax.jms.JMSException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import javax.xml.stream.XMLStreamException;
 
 /**
  * Tests JMS transactions with inbound endpoints.
  */
 public class JMSInboundTransactionTestCase extends ESBIntegrationTest {
 
-	private LogViewerClient logViewerClient = null;
-	private ActiveMQServer activeMQServer = new ActiveMQServer();
-	String message;
+    private LogViewerClient logViewerClient = null;
+    private ActiveMQServer activeMQServer = new ActiveMQServer();
+    String message;
 
-	@BeforeClass(alwaysRun = true)
-	protected void init() throws Exception {
+    @BeforeClass(alwaysRun = true)
+    protected void init() throws Exception {
 
-		activeMQServer.startJMSBroker();
-		super.init();
+        activeMQServer.startJMSBroker();
+        super.init();
 
-		OMElement synapse =
-				esbUtils.loadResource(
-						"/artifacts/ESB/jms/inbound/transport/jms_inbound_transaction.xml");
-		updateESBConfiguration(synapse);
-		message = String.valueOf(
-				esbUtils.loadResource("artifacts/ESB/jms/inbound/transport/message.xml"));
-		logViewerClient = new LogViewerClient(contextUrls.getBackEndUrl(), getSessionCookie());
-	}
+        verifySequenceExistence("jmsInboundEpTransactionRollbackTestInSequence");
+        verifySequenceExistence("jmsInboundEpTransactionRollbackTestFaultSequence");
+        verifySequenceExistence("jmsInboundEndpointTransactionRequestHandlerSequence");
 
-	@Test(groups = { "wso2.esb" }, description = "Successfully committing the message" , enabled = false)
-	public void testTransactionCommit() throws Exception {
+        message = String.valueOf(loadResource("message.xml"));
+        logViewerClient = new LogViewerClient(contextUrls.getBackEndUrl(), getSessionCookie());
+    }
 
-		int beforeLogCount;
-		boolean successValue = false;
-		String queueName = "localq";
-		LogEvent[] logs;
-		JMSQueueMessageProducer sender =
-				new JMSQueueMessageProducer(
-						JMSBrokerConfigurationProvider.getInstance().getBrokerConfiguration());
+    /**
+     * Tests whether committing a transaction removes the message from the queue.
+     *
+     * @throws Exception for any unexpected exception
+     */
+    @Test(groups = { "wso2.esb" },
+          description = "Successfully committing the message")
+    public void testTransactionCommit() throws Exception {
 
-		beforeLogCount = logViewerClient.getAllRemoteSystemLogs().length;
-		try {
-			sender.connect(queueName);
-			sender.pushMessage(message);
-		} finally {
-			sender.disconnect();
-		}
-		try {
-			addInboundEndpoint(esbUtils.loadResource(
-					"artifacts/ESB/jms/inbound/transport/jms_commit_synapse.xml"));
-			Thread.sleep(10000);
-		} catch (JMSException e) {
-			log.info(e + ", Error while adding the inbound endpoint");
-		}
-		logs = logViewerClient.getAllRemoteSystemLogs();
-		for (int i = 0; i < (logs.length - beforeLogCount); i++) {
-			if (logs[i].getMessage().contains("Committed")) {
-				successValue = checkForQueue("Committed");
-				break;
-			}
-		}
-		Assert.assertTrue(successValue, "Error while performing commit");
-		deleteInboundEndpoints();
-		Thread.sleep(3000);
-	}
+        String queueName = "testTransactionCommitQueue";
+        JMSQueueMessageProducer sender = new JMSQueueMessageProducer(JMSBrokerConfigurationProvider.getInstance()
+                                                                                                   .getBrokerConfiguration());
 
-	@Test(groups = { "wso2.esb" }, description = "Rolling back the failed message to the queue" , enabled = false)
-	public void testTransactionRollBack() throws Exception {
+        logViewerClient.clearLogs();
 
-		boolean successValue;
-		String queueName = "localq";
-		JMSQueueMessageProducer sender =
-				new JMSQueueMessageProducer(
-						JMSBrokerConfigurationProvider.getInstance().getBrokerConfiguration());
-		try {
-			sender.connect(queueName);
-			sender.pushMessage(message);
-		} finally {
-			sender.disconnect();
-		}
-		addInboundEndpoint(esbUtils.loadResource(
-				"artifacts/ESB/jms/inbound/transport/jms_rollback_synapse.xml"));
-		Thread.sleep(8000);
-		successValue = checkForQueue("Rollbacked");
-		Assert.assertTrue(successValue, "Error while performing rollback");
-		deleteInboundEndpoints();
-		Thread.sleep(3000);
-	}
+        try {
+            sender.connect(queueName);
+            sender.pushMessage(message);
+        } finally {
+            sender.disconnect();
+        }
 
-	private boolean checkForQueue(String status) throws Exception {
+        addInboundEndpoint(loadResource("jmsInboundTransactionCommitInboundEp.xml"));
 
-		String poppedMessage = null;
-		JMSQueueMessageConsumer consumer = new JMSQueueMessageConsumer(
-				JMSBrokerConfigurationProvider.getInstance().getBrokerConfiguration());
-		try {
-			consumer.connect("localq");
-			poppedMessage = consumer.popMessage();
-		} finally {
-			consumer.disconnect();
-		}
-		if (status.equals("Committed") && poppedMessage == null) {
-			return true;
-		} else if (status.equals("Rollbacked") && poppedMessage != null) {
-			return true;
-		}
-		return false;
-	}
+        // Wait till the message is picked from inbound endpoint
+        boolean committed = Utils.checkForLog(logViewerClient, "Committed", 8);
 
-	@AfterClass(alwaysRun = true)
-	public void destroy() throws Exception {
-		super.cleanup();
-		Thread.sleep(3000);
-		activeMQServer.stopJMSBroker();
+        Assert.assertTrue(committed, "Did not find the \"Committed\" log");
+        deleteInboundEndpoints();
 
-	}
+        Assert.assertTrue(isQueueEmpty(queueName), "Queue (" + queueName + ") should be empty after commit");
+    }
+
+    /**
+     * Tests whether rolling back a transaction does not removes the message from the queue.
+     *
+     * @throws Exception
+     */
+    @Test(groups = { "wso2.esb" },
+          description = "Rolling back the failed message to the queue")
+    public void testTransactionRollBack() throws Exception {
+
+        String queueName = "testTransactionRollBackQueue";
+        JMSQueueMessageProducer sender = new JMSQueueMessageProducer(JMSBrokerConfigurationProvider.getInstance()
+                                                                                                   .getBrokerConfiguration());
+        logViewerClient.clearLogs();
+
+        try {
+            sender.connect(queueName);
+            sender.pushMessage(message);
+        } finally {
+            sender.disconnect();
+        }
+
+        addInboundEndpoint(loadResource("jmsInboundTransactionRollbackInboundEp.xml"));
+        boolean rollbacked = Utils.checkForLog(logViewerClient, "Rollbacked", 8);
+
+        Assert.assertTrue(rollbacked, "Did not find the \"Rollbacked\" log");
+        deleteInboundEndpoints();
+
+        Assert.assertFalse(isQueueEmpty(queueName), "Queue (" + queueName + ") should not be empty after rollback");
+    }
+
+    private boolean isQueueEmpty(String queueName) throws Exception {
+
+        String poppedMessage;
+        JMSQueueMessageConsumer consumer = new JMSQueueMessageConsumer(
+                JMSBrokerConfigurationProvider.getInstance().getBrokerConfiguration());
+        try {
+            consumer.connect(queueName);
+            poppedMessage = consumer.popMessage();
+        } finally {
+            consumer.disconnect();
+        }
+
+        return poppedMessage == null;
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void destroy() throws Exception {
+        super.cleanup();
+        activeMQServer.stopJMSBroker();
+
+    }
+
+    private OMElement loadResource(String resourceName) throws FileNotFoundException, XMLStreamException {
+        return esbUtils.loadResource(
+                "artifacts" + File.separator + "ESB" + File.separator + "jms" + File.separator + "inbound"
+                        + File.separator + "transport" + File.separator + resourceName);
+    }
 
 }
