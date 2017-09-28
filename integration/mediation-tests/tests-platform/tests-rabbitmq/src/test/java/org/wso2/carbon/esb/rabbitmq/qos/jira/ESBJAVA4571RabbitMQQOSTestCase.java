bce409f2b8a654a5b2654bdd0b0ae504b6d9d99a
sdkottegoda <sdkottegoda@gmail.com>
2017-09-28 14:17:25 +0530

    Fix and enable ESBJAVA4571RabbitMQQOSTestCase

--- integration/mediation-tests/tests-platform/tests-rabbitmq/src/test/java/org/wso2/carbon/esb/rabbitmq/qos/jira/ESBJAVA4571RabbitMQQOSTestCase.java
+++ integration/mediation-tests/tests-platform/tests-rabbitmq/src/test/java/org/wso2/carbon/esb/rabbitmq/qos/jira/ESBJAVA4571RabbitMQQOSTestCase.java
@@ -21,80 +21,44 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.wso2.carbon.automation.engine.context.AutomationContext;
-import org.wso2.carbon.automation.engine.context.TestUserMode;
-import org.wso2.carbon.esb.rabbitmq.utils.RabbitMQTestUtils;
-import org.wso2.carbon.integration.common.utils.exceptions.AutomationUtilException;
-import org.wso2.esb.integration.common.utils.common.ServerConfigurationManager;
+import org.wso2.carbon.esb.rabbitmq.utils.RabbitMQServerInstance;
 import org.wso2.esb.integration.common.clients.proxy.admin.ProxyServiceAdminClient;
 import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
 import org.wso2.esb.integration.common.utils.clients.rabbitmqclient.RabbitMQConsumerClient;
 import org.wso2.esb.integration.common.utils.clients.rabbitmqclient.RabbitMQProducerClient;
-import org.wso2.esb.integration.common.utils.servers.RabbitMQServer;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 /**
- * This class is to test rabbitMQ qos settings applied correctly or not
+ * This class is to test rabbitMQ qos settings applied correctly or not.
  */
 public class ESBJAVA4571RabbitMQQOSTestCase extends ESBIntegrationTest {
 
     private RabbitMQProducerClient sender;
     private ProxyServiceAdminClient proxyServiceAdminClient;
-    private RabbitMQServer rabbitMQServer;
-    private ServerConfigurationManager configurationManagerAxis2;
 
     @BeforeClass(alwaysRun = true)
     public void init() throws Exception {
         super.init();
-        configurationManagerAxis2 =
-                new ServerConfigurationManager(new AutomationContext("ESB", TestUserMode.SUPER_TENANT_ADMIN));
-        File customAxisConfigAxis2 = new File(getESBResourceLocation() + File.separator +
-                                              "axis2config" + File.separator + "axis2.xml");
-        configurationManagerAxis2.applyConfiguration(customAxisConfigAxis2);
-        super.init();
-
-        rabbitMQServer = RabbitMQTestUtils.getRabbitMQServerInstance();
-        //This is to stop existing rabbitMQ server instances
-        rabbitMQServer.stop();
-        rabbitMQServer.start();
-        Assert.assertTrue(rabbitMQServer.isRabbitMQStarted(90), "Failed to start rabbitMQ server properly within given timeout");
-        initRabbitMQBroker();
-        loadESBConfigurationFromClasspath("/artifacts/ESB/qos/rabbitMQ/RabbitMQQOSProxy.xml");
+        sender = RabbitMQServerInstance.createProducerWithDeclaration("qosExchange", "qosQueue");
+        loadESBConfigurationFromClasspath(File.separator + "artifacts" + File.separator
+                                          + "ESB" + File.separator + "qos" + File.separator + "rabbitMQ"
+                                          + File.separator
+                                          + "RabbitMQQOSProxy.xml");
         proxyServiceAdminClient = new ProxyServiceAdminClient(contextUrls.getBackEndUrl(), getSessionCookie());
-    }
-
-    private void initRabbitMQBroker() throws Exception{
-        //Retrying rabbitMQ connection until timeout happens
-        for (int i = 1; i * 5 < 90; i++) {
-            sender = new RabbitMQProducerClient("localhost", 5672, "guest", "guest");
-            try {
-                sender.declareAndConnect("qosExchange", "qosQueue");
-                log.info("Successfully connected to rabbitMQ broker in " + i + " attempt");
-                return;
-            } catch (IOException e) {
-                log.warn("Error connecting to the broker in " + i + " attempt - " + e.getMessage(), e);
-                Thread.sleep(300);//sleep for 5 seconds
-            }
-        }
-        Assert.fail("Could not connect to RabbitMQ broker");
     }
 
     @Test(groups = {"wso2.esb"}, description = "Test ESB as a RabbitMQ consumer QOS applied")
     public void testRabbitMQQOSConsumer() throws Exception {
 
         proxyServiceAdminClient.stopProxyService("RabbitMQQOSProxy");
-        RabbitMQConsumerClient consumer = new RabbitMQConsumerClient("localhost");
+        RabbitMQConsumerClient consumer = RabbitMQServerInstance.createConsumerWithDeclaration("qosExchange",
+                                                                                               "qosQueue");
 
-        try {
-            consumer.declareAndConnect("qosExchange", "qosQueue");
-        } catch (IOException e) {
-            Assert.fail("Could not connect to RabbitMQ broker");
-        }
-
-        consumer.popAllMessages(); //this is to remove already existing messages
+        //remove already existing messages
+        consumer.popAllMessages();
 
         try {
             sender.declareAndConnect("qosExchange", "qosQueue");
@@ -131,12 +95,5 @@ public class ESBJAVA4571RabbitMQQOSTestCase extends ESBIntegrationTest {
     @AfterClass(alwaysRun = true)
     public void end() throws Exception {
         super.cleanup();
-        try {
-            rabbitMQServer.stop();
-            sender = null;
-            configurationManagerAxis2.restoreToLastConfiguration();
-        } catch (AutomationUtilException e) {
-            log.info("Error cleaning up - " + e.getMessage(), e);
-        }
     }
 }
