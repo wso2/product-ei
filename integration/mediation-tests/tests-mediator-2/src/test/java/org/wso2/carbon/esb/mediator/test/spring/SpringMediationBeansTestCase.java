7e9e6ec006cb09d8fb6c70b2feca03a484275df6
malakaganga <malakaganga.ac@gmail.com>
2017-10-03 11:02:55 +0530

    Refactor Spring Mediator Test
    Remove deletion of main sequence.

--- integration/mediation-tests/tests-mediator-2/src/test/java/org/wso2/carbon/esb/mediator/test/spring/SpringMediationBeansTestCase.java
+++ integration/mediation-tests/tests-mediator-2/src/test/java/org/wso2/carbon/esb/mediator/test/spring/SpringMediationBeansTestCase.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.esb.mediator.test.spring;
 
 import org.apache.axis2.AxisFault;
@@ -23,8 +40,7 @@ import javax.activation.DataHandler;
  * This class tests whether the SpringMediator throws proper error while the 
  * custom java class having any problem. 
  */
-
-public class ESBJAVA3291TestCase extends ESBIntegrationTest {
+public class SpringMediationBeansTestCase extends ESBIntegrationTest {
 
 	private final String SPRING_XML_LOCATION = File.separator + "artifacts"
 			+ File.separator + "ESB" + File.separator + "mediatorconfig"
@@ -46,7 +62,6 @@ public class ESBJAVA3291TestCase extends ESBIntegrationTest {
 	@AfterClass(alwaysRun = true)
 	public void destroy() throws Exception {
 		try {
-			deleteSequence("main");
 			clearUploadedResource();
 		} finally {
             super.cleanup();
