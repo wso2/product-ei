5e7fb91734735c829b3ea3c5a2df7acf91b2e166
Himasha Guruge <himashag@wso2.com>
2017-10-17 16:31:51 +0530

    Additional coverage for EndpointTemplateAdminService

--- integration/mediation-tests/tests-service/src/test/java/org/wso2/carbon/esb/rest/test/api/TemplateAdminService.java
+++ integration/mediation-tests/tests-service/src/test/java/org/wso2/carbon/esb/rest/test/api/TemplateAdminService.java
@@ -24,6 +24,7 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.wso2.esb.integration.common.clients.template.EndpointTemplateAdminServiceClient;
 import org.wso2.esb.integration.common.clients.template.SequenceTemplateAdminServiceClient;
 import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
 
@@ -31,7 +32,9 @@ import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
  * Test templateAdminService related operations
  */
 public class TemplateAdminService extends ESBIntegrationTest {
+
     private SequenceTemplateAdminServiceClient templateAdmin;
+    private EndpointTemplateAdminServiceClient endpointAdmin;
     private static final String templateName = "TemplateAdminService";
     private  static final String dynamicName = "TemplateUserService";
     private final String KEY = "conf:/template/dynamicAdminTemplate";
@@ -44,10 +47,28 @@ public class TemplateAdminService extends ESBIntegrationTest {
     private static final String template3 =
             "<template  xmlns=\"http://ws.apache.org/ns/synapse\" name=\"TemplateUserService\">\n" + "<parameter name=\"message\"/>\n" + " <sequence>\n" + "<log level=\"custom\">\n"
                     + "<property name=\"TMPL_MSG \" value= \"in Template Service Updated..\" />\n" + "</log>\n" + "   </sequence>\n" + "</template>";
+
+    private static final String endpointTmpl1 = "<template xmlns=\"http://ws.apache.org/ns/synapse\" name= \"SampleEndpointTemplate\">" +
+            "<axis2ns3:parameter xmlns:axis2ns3=\"http://ws.apache.org/ns/synapse\" name=\"name\" />" +
+            "<axis2ns4:parameter xmlns:axis2ns4=\"http://ws.apache.org/ns/synapse\" name=\"uri\" />" +
+            "<endpoint name=\"quoteEndpoint\">" +
+            "<address uri=\"https://localhost:9000/services/SimpleStockQuoteService\">" +
+            "</address>" +
+            "</endpoint>" +
+            "</template>";
+    private static final String endpointTmpl2 = "<template xmlns=\"http://ws.apache.org/ns/synapse\" name= \"SampleEndpointTemplate\">" +
+            "<endpoint name=\"updateEndpoint\">" +
+            "<address uri=\"https://localhost:9000/services/SimpleStockQuoteService\">" +
+            "</address>" +
+            "</endpoint>" +
+            "</template>";
+    private static final String endpointName = "SampleEndpointTemplate";
+
     @BeforeClass(alwaysRun = true)
     public void init() throws Exception {
         super.init();
         templateAdmin = new SequenceTemplateAdminServiceClient(contextUrls.getBackEndUrl(), getSessionCookie());
+        endpointAdmin = new EndpointTemplateAdminServiceClient(contextUrls.getBackEndUrl(), getSessionCookie());
     }
 
     @Test(groups = { "wso2.esb" },
@@ -57,7 +78,7 @@ public class TemplateAdminService extends ESBIntegrationTest {
         int prevCount = templateAdmin.getTemplatesCount();
         templateAdmin.addSequenceTemplate(AXIOMUtil.stringToOM(template1));
         int latestCount = templateAdmin.getTemplatesCount();
-        org.testng.Assert.assertTrue(prevCount < latestCount, "New template not added");
+        Assert.assertTrue(prevCount < latestCount, "New template not added");
     }
 
     @Test(groups = { "wso2.esb" },
@@ -145,7 +166,7 @@ public class TemplateAdminService extends ESBIntegrationTest {
         int prevCount = templateAdmin.getDynamicTemplateCount();
         templateAdmin.addDynamicSequenceTemplate(KEY,AXIOMUtil.stringToOM(template1));
         int latestCount = templateAdmin.getDynamicTemplateCount();
-        org.testng.Assert.assertTrue(prevCount < latestCount, "Dynamic template not added");
+        Assert.assertTrue(prevCount < latestCount, "Dynamic template not added");
     }
 
     @Test(groups = { "wso2.esb" },
@@ -161,16 +182,61 @@ public class TemplateAdminService extends ESBIntegrationTest {
           description = "Test dynamic template creation service",
           priority = 12)
     public void testUpdateDynamicTemplate() throws Exception {
-        templateAdmin.updateDynamicTemplate(KEY,AXIOMUtil.stringToOM(template3));
-        templateAdmin.saveDynamicTemplate(KEY,AXIOMUtil.stringToOM(template3));
+        templateAdmin.updateDynamicTemplate(KEY, AXIOMUtil.stringToOM(template3));
+        templateAdmin.saveDynamicTemplate(KEY, AXIOMUtil.stringToOM(template3));
         OMElement elm = templateAdmin.getDynamicTemplate(KEY);
         Assert.assertTrue(elm.toString().contains(dynamicName), "Dynamic template not updated");
 
     }
 
+    @Test(groups = { "wso2.esb" },
+          description = "Test endpoint template creation service",
+          priority = 13)
+    public void testCreateEndpointTemplate() throws Exception {
+        int prevCount = endpointAdmin.getEndpointTemplatesCount();
+        endpointAdmin.addEndpointTemplate(AXIOMUtil.stringToOM(endpointTmpl1));
+        int latestCount = endpointAdmin.getEndpointTemplatesCount();
+        Assert.assertTrue(prevCount < latestCount, "New endpoint template not added");
+    }
+
+    @Test(groups = { "wso2.esb" },
+          description = "Test endpoint template list service",
+          priority = 14)
+    public void testListEndpointTemplate() throws Exception {
+        OMElement elm = endpointAdmin.getEndpointTemplate(endpointName);
+        Assert.assertNotNull(elm, "Unable to get requested endpoint template");
+        Assert.assertTrue(elm.toString().contains(endpointName), "New endpoint template not listed");
+    }
+
+    @Test(groups = { "wso2.esb" },
+          description = "Test endpoint template update service",
+          priority = 15)
+    public void testUpdateEndpointTemplate() throws Exception {
+        if (!endpointAdmin.hasDuplicateEndpointTemplate(endpointTmpl1)) {
+            endpointAdmin.saveEndpointTemplate(endpointTmpl2);
+            OMElement elm = endpointAdmin.getEndpointTemplate(endpointName);
+            Assert.assertTrue(elm.toString().contains("updateEndpoint"), "Endpoint template not updated");
+        }
+    }
+
+    @Test(groups = { "wso2.esb" },
+          description = "Test listing invalid endpoint template",
+          priority = 14)
+    public void testListInvalidEndpointTemplate() throws Exception {
+        try {
+            endpointAdmin.getEndpointTemplate("invalidEP");
+            Assert.fail("Expected exception not thrown for invalid endpoint template");
+        } catch (AxisFault e) {
+            Assert.assertTrue(
+                    e.getMessage().contains("Couldn't get the Synapse Configuration to get the Endpoint Template"),
+                    "Expected exception message not available");
+        }
+    }
+
     @AfterClass(alwaysRun = true)
     public void close() throws Exception {
         templateAdmin = null;
+        endpointAdmin = null;
         super.cleanup();
     }
 
