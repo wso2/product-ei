9b5b34bbb9ebae3ece7f45a91fcb7c35dc795a4e
isudana <isudana@gmail.com>
2017-11-22 15:08:45 +0530

    Add execution scripts for integrator profile

---
+++ distribution/src/scripts/integrator.bat
@@ -0,0 +1,28 @@
+@echo off
+
+REM ---------------------------------------------------------------------------
+REM   Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+REM
+REM   Licensed under the Apache License, Version 2.0 (the "License");
+REM   you may not use this file except in compliance with the License.
+REM   You may obtain a copy of the License at
+REM
+REM   http://www.apache.org/licenses/LICENSE-2.0
+REM
+REM   Unless required by applicable law or agreed to in writing, software
+REM   distributed under the License is distributed on an "AS IS" BASIS,
+REM   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+REM   See the License for the specific language governing permissions and
+REM   limitations under the License.
+
+rem ---------------------------------------------------------------------------
+rem Main Script for Integrator
+rem
+rem Environment Variable Prerequisites
+rem
+rem   JAVA_HOME       Must point at your Java Development Kit installation.
+rem
+rem   JAVA_OPTS       (Optional) Java runtime options used when the commands
+rem                   is executed.
+rem ---------------------------------------------------------------------------
+..\wso2\ballerina\bin\ballerina.bat run %1
