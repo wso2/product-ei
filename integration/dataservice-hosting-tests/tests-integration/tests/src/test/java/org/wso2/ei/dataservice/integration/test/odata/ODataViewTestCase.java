73a61bbce7a0547c4a410be7aab2cd7164c87acf
madhawa-gunasekara <madhawag@wso2.com>
2017-12-21 21:10:45 -0600

    Fix Indentation

--- integration/dataservice-hosting-tests/tests-integration/tests/src/test/java/org/wso2/ei/dataservice/integration/test/odata/ODataViewTestCase.java
+++ integration/dataservice-hosting-tests/tests-integration/tests/src/test/java/org/wso2/ei/dataservice/integration/test/odata/ODataViewTestCase.java
@@ -18,27 +18,13 @@
 
 package org.wso2.ei.dataservice.integration.test.odata;
 
-import ca.uhn.hl7v2.model.v21.datatype.ST;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPatch;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.wso2.ei.dataservice.integration.test.DSSIntegrationTest;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -48,22 +34,22 @@ import java.util.Map;
  * This class contains OData View Test case, Where you can retrieve data from a VIEW.
  */
 public class ODataViewTestCase extends DSSIntegrationTest {
-	private final String serviceName = "ODataSampleSuperTenantService";
-	private final String configId = "default";
-	private final String viewName = "USACUSTOMERS";
-	private String webAppUrl;
+    private final String serviceName = "ODataSampleSuperTenantService";
+    private final String configId = "default";
+    private final String viewName = "USACUSTOMERS";
+    private String webAppUrl;
 
-	@BeforeClass(alwaysRun = true)
-	public void serviceDeployment() throws Exception {
-		super.init();
-		List<File> sqlFileLis = new ArrayList<>();
-		sqlFileLis.add(selectSqlFile("CreateODataTables.sql"));
-		sqlFileLis.add(selectSqlFile("Customers.sql"));
-		deployService(serviceName,
-		              createArtifact(getResourceLocation() + File.separator + "dbs" + File.separator + "odata" +
-		                             File.separator + "ODataSampleSuperTenantService.dbs", sqlFileLis));
-		webAppUrl = dssContext.getContextUrls().getWebAppURL();
-	}
+    @BeforeClass(alwaysRun = true)
+    public void serviceDeployment() throws Exception {
+        super.init();
+        List<File> sqlFileLis = new ArrayList<>();
+        sqlFileLis.add(selectSqlFile("CreateODataTables.sql"));
+        sqlFileLis.add(selectSqlFile("Customers.sql"));
+        deployService(serviceName,
+                createArtifact(getResourceLocation() + File.separator + "dbs" + File.separator + "odata" +
+                        File.separator + "ODataSampleSuperTenantService.dbs", sqlFileLis));
+        webAppUrl = dssContext.getContextUrls().getWebAppURL();
+    }
 
     @AfterClass(alwaysRun = true)
     public void destroy() throws Exception {
@@ -71,19 +57,19 @@ public class ODataViewTestCase extends DSSIntegrationTest {
         cleanup();
     }
 
-	@Test(groups = { "wso2.dss" }, description = "OData View Test")
-	public void validateViewTestCase() throws Exception {
-		String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/" + viewName;
-		Map<String, String> headers = new HashMap<>();
-		headers.put("Accept", "application/json");
-		Object[] response = ODataTestUtils.sendGET(endpoint, headers);
-		Assert.assertEquals(response[0], ODataTestUtils.OK);
-		endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/";
-		response = ODataTestUtils.sendGET(endpoint, headers);
-		Assert.assertEquals(response[0], ODataTestUtils.OK);
-		Assert.assertTrue(response[1].toString().contains("USA"));
-		Assert.assertTrue(!response[1].toString().contains("France"));
-		Assert.assertTrue(!response[1].toString().contains("Finland"));
-	}
+    @Test(groups = {"wso2.dss"}, description = "OData View Test")
+    public void validateViewTestCase() throws Exception {
+        String endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/" + viewName;
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Accept", "application/json");
+        Object[] response = ODataTestUtils.sendGET(endpoint, headers);
+        Assert.assertEquals(response[0], ODataTestUtils.OK);
+        endpoint = webAppUrl + "/odata/" + serviceName + "/" + configId + "/";
+        response = ODataTestUtils.sendGET(endpoint, headers);
+        Assert.assertEquals(response[0], ODataTestUtils.OK);
+        Assert.assertTrue(response[1].toString().contains("USA"));
+        Assert.assertTrue(!response[1].toString().contains("France"));
+        Assert.assertTrue(!response[1].toString().contains("Finland"));
+    }
 
 }
