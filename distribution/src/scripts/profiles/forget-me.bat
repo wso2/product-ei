5c277148be6b60ef758cc66c9fe55daa839a233d
Milan Perera <milan@wso2.com>
2018-02-22 11:17:31 +0530

    Ship identity-anonymization-tool with EI

---
+++ distribution/src/scripts/profiles/forget-me.bat
@@ -0,0 +1,40 @@
+@echo off
+rem Copyright 2005-2018 WSO2, Inc. (http://wso2.com)
+rem Licensed to the Apache Software Foundation (ASF) under one
+rem or more contributor license agreements.  See the NOTICE file
+rem distributed with this work for additional information
+rem regarding copyright ownership.  The ASF licenses this file
+rem to you under the Apache License, Version 2.0 (the
+rem "License"); you may not use this file except in compliance
+rem with the License.  You may obtain a copy of the License at
+rem
+rem    http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing,
+rem software distributed under the License is distributed on an
+rem  # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+rem KIND, either express or implied.  See the License for the
+rem specific language governing permissions and limitations
+rem under the License.
+
+rem ---------------------------------------------------------------------------
+rem Startup script for the ciphertool
+rem
+rem Environment Variable Prerequisites
+rem
+rem   CARBON_HOME      Must point at your CARBON directory
+rem
+rem   JAVA_HOME       Must point at your Java Development Kit installation.
+rem
+rem   JAVA_OPTS       (Optional) Java runtime options
+rem ---------------------------------------------------------------------------
+
+:checkServer
+setlocal enabledelayedexpansion
+rem %~sdp0 is expanded pathname of the current script under NT with spaces in the path removed
+if "%CARBON_HOME%"=="" set CARBON_HOME=%~sdp0..
+SET curDrive=%cd:~0,1%
+SET wsasDrive=%CARBON_HOME:~0,1%
+if not "%curDrive%" == "%wsasDrive%" %wsasDrive%:
+cd %CARBON_HOME%
+call %CARBON_HOME%\..\tools\identity-anonymization-tool\bin\forget-me.bat
