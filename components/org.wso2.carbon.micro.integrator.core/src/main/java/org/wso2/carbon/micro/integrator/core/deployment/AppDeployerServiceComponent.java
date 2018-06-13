a018bd8ac3bc6170b6f2e7f6577527cf457a1e6a
madhawa-gunasekara <madhawag@wso2.com>
2018-06-14 00:19:47 +0530

    Fix Name change

--- components/org.wso2.carbon.micro.integrator.core/src/main/java/org/wso2/carbon/micro/integrator/core/deployment/AppDeployerServiceComponent.java
+++ components/org.wso2.carbon.micro.integrator.core/src/main/java/org/wso2/carbon/micro/integrator/core/deployment/AppDeployerServiceComponent.java
@@ -112,7 +112,7 @@ public class AppDeployerServiceComponent {
         DeploymentService deploymentService = new DeploymentServiceImpl(artifactDeploymentManager, cAppDeploymentManager);
         ctxt.getBundleContext().registerService(DeploymentService.class.getName(),deploymentService, null);
 
-        log.debug("micro-integrator artifact/Capp Deployment completed");
+        log.debug("microIntegrator artifact/Capp Deployment completed");
     }
 
     protected void deactivate(ComponentContext ctxt) {
