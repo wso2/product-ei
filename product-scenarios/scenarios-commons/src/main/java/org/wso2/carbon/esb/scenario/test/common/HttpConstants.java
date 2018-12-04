45048ed83de5d123e8168f2f8996cbf46592ae50
dulanjalidilmi <dulanjalidilmi@gmail.com>
2018-12-04 17:17:04 +0530

    Add testcases to product scenarios

---
+++ product-scenarios/scenarios-commons/src/main/java/org/wso2/carbon/esb/scenario/test/common/HttpConstants.java
@@ -0,0 +1,8 @@
+package org.wso2.carbon.esb.scenario.test.common;
+
+public class HttpConstants {
+
+    public static final String MEDIA_TYPE_APPLICATION_XML = "application/xml";
+    public static final String MEDIA_TYPE_APPLICATION_JSON = "application/json";
+
+}
