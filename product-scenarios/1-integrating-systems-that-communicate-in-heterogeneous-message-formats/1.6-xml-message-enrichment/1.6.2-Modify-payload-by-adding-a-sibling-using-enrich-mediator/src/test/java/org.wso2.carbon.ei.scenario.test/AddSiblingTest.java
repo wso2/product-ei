2fc42f1dbd23d155b10be7bdab522c2a0981bfca
Isuru Uyanage <isuruuy@gmail.com>
2019-01-11 13:59:21 +0530

    add test fot 1.6.2.4

--- product-scenarios/1-integrating-systems-that-communicate-in-heterogeneous-message-formats/1.6-xml-message-enrichment/1.6.2-Modify-payload-by-adding-a-sibling-using-enrich-mediator/src/test/java/org.wso2.carbon.ei.scenario.test/AddSiblingTest.java
+++ product-scenarios/1-integrating-systems-that-communicate-in-heterogeneous-message-formats/1.6-xml-message-enrichment/1.6.2-Modify-payload-by-adding-a-sibling-using-enrich-mediator/src/test/java/org.wso2.carbon.ei.scenario.test/AddSiblingTest.java
@@ -115,6 +115,45 @@ public class AddSiblingTest extends ScenarioTestBase {
                 "urn:mediate", "AddSiblingToTargetBody");
     }
 
+    //This test is to verify if payload can be modified by adding an element to the payload as a sibling which is stored in a property.
+    @Test(description = "1.6.2.4 - Adding an element to the payload as a sibling which is stored in a property")
+    public void AddSiblingMessageBodyStoredInProperty() throws IOException, XMLStreamException {
+        String url = getProxyServiceURLHttp("1_6_2_4_Proxy_Add_Sibling_toMessageBodyStoredInProperty");
+
+        String request =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                        + "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://services.samples\" xmlns:xsd=\"http://services.samples/xsd\">\n"
+                        + "   <soapenv:Header />\n"
+                        + "   <soapenv:Body>\n"
+                        + "      <ser:getQuote>\n"
+                        + "         <!--Optional:-->\n"
+                        + "         <ser:request>\n"
+                        + "            <!--Optional:-->\n"
+                        + "            <xsd:symbol>WSO2</xsd:symbol>\n"
+                        + "         </ser:request>\n"
+                        + "      </ser:getQuote>\n"
+                        + "   </soapenv:Body>\n"
+                        + "</soapenv:Envelope>";
+
+        String expectedResponse =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                        + "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ser=\"http://services.samples\" xmlns:xsd=\"http://services.samples/xsd\">\n"
+                        + "   <soapenv:Body>\n"
+                        + "      <ser:getQuote>\n"
+                        + "         <!--Optional:-->\n"
+                        + "         <ser:request>\n"
+                        + "            <!--Optional:-->\n"
+                        + "            <xsd:symbol>WSO2</xsd:symbol>\n"
+                        + "            <productid>IC002</productid>\n"
+                        + "         </ser:request>\n"
+                        + "      </ser:getQuote>\n"
+                        + "   </soapenv:Body>\n"
+                        + "</soapenv:Envelope>";
+
+        HTTPUtils.invokeSoapActionAndAssert(url, request, ScenarioConstants.MESSAGE_ID, expectedResponse, 200,
+                "urn:mediate", "AddSiblingMessageBodyStoredInProperty");
+    }
+
     @AfterClass(description = "Server Cleanup",
                 alwaysRun = true)
     public void cleanup() throws Exception {
