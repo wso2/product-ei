318f0ae29dd21593180d09aa81da2ce5ccf0862f
Dilmi Dulanjali <38608578+dulanjalidilmi@users.noreply.github.com>
2019-01-18 09:34:33 +0530

    Skip tests for incompactible product versions (#3099)

--- product-scenarios/5-route-messages-between-systems/5.3-load-balance-messages-among-systems/5.3.1-load-balance-messages-received-over-http/5.3.1.1-load-balance-with-load-balance-group/src/test/java/org/wso2/carbon/ei/scenario/test/LoadBalanceEndpointsWithLoadBalanceGroupTest.java
+++ product-scenarios/5-route-messages-between-systems/5.3-load-balance-messages-among-systems/5.3.1-load-balance-messages-received-over-http/5.3.1.1-load-balance-with-load-balance-group/src/test/java/org/wso2/carbon/ei/scenario/test/LoadBalanceEndpointsWithLoadBalanceGroupTest.java
@@ -46,7 +46,8 @@ public class LoadBalanceEndpointsWithLoadBalanceGroupTest extends ScenarioTestBa
 
     @BeforeClass()
     public void init() throws Exception {
-        skipTestsForIncompatibleProductVersions(ScenarioConstants.VERSION_490);
+        skipTestsForIncompatibleProductVersions(ScenarioConstants.VERSION_490, ScenarioConstants.VERSION_600,
+                                                ScenarioConstants.VERSION_610, ScenarioConstants.VERSION_611);
         super.init();
     }
 
