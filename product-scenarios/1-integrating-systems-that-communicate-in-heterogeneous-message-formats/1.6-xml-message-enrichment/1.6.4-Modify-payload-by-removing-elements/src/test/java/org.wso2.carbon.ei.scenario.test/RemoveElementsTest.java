50579da4e806a912a6e9685a66908d4d2672abc6
Isuru Uyanage <isuruuy@gmail.com>
2019-01-25 18:28:17 +0530

    add tests to 1.6.4.3

--- product-scenarios/1-integrating-systems-that-communicate-in-heterogeneous-message-formats/1.6-xml-message-enrichment/1.6.4-Modify-payload-by-removing-elements/src/test/java/org.wso2.carbon.ei.scenario.test/RemoveElementsTest.java
+++ product-scenarios/1-integrating-systems-that-communicate-in-heterogeneous-message-formats/1.6-xml-message-enrichment/1.6.4-Modify-payload-by-removing-elements/src/test/java/org.wso2.carbon.ei.scenario.test/RemoveElementsTest.java
@@ -21,7 +21,6 @@ package org.wso2.carbon.ei.scenario.test;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-import org.wso2.carbon.esb.scenario.test.common.ScenarioConstants;
 import org.wso2.carbon.esb.scenario.test.common.http.HTTPUtils;
 import org.wso2.carbon.esb.scenario.test.common.ScenarioTestBase;
 
@@ -44,7 +43,7 @@ public class RemoveElementsTest extends ScenarioTestBase {
     @Test(description = "1.6.4.1")
     public void removeElementsUsingScriptMediator() throws IOException, XMLStreamException {
         String url = getProxyServiceURLHttp("1_6_4_1_Proxy_RemoveElements_ScriptMediator");
-
+        String testCaseId = "1.6.4.1";
         String request =
                 "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
                 + "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
@@ -66,15 +65,15 @@ public class RemoveElementsTest extends ScenarioTestBase {
                 + "   </soapenv:Body>\n"
                 + "</soapenv:Envelope>";
 
-        HTTPUtils.invokeSoapActionAndAssert(url, request, ScenarioConstants.MESSAGE_ID, expectedResponse, 200,
-                "urn:mediate", "RemoveElementsUsingScriptMediator");
+        HTTPUtils.invokeSoapActionAndAssert(url, request, testCaseId, expectedResponse, 200,
+                "urn:mediate", "removeElementsUsingScriptMediator");
     }
 
     //This test is to verify if payload can be modified by removing content of an element using enrich mediator.
     @Test(description = "1.6.4.2")
     public void removeContentOfElementUsingEnrichMediator() throws IOException, XMLStreamException {
         String url = getProxyServiceURLHttp("1_6_4_2_Proxy_RemoveContentofElements_EnrichMediator");
-
+        String testCaseId = "1.6.4.2";
         String request =
                 "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
                 + "   <soapenv:Header />\n"
@@ -102,8 +101,44 @@ public class RemoveElementsTest extends ScenarioTestBase {
                         + "   </soapenv:Body>"
                         + "</soapenv:Envelope>";
 
-        HTTPUtils.invokeSoapActionAndAssert(url, request, ScenarioConstants.MESSAGE_ID, expectedResponse, 200,
-                "urn:mediate", "RemoveContentOfElementUsingEnrichMediator");
+        HTTPUtils.invokeSoapActionAndAssert(url, request, testCaseId, expectedResponse, 200,
+                "urn:mediate", "removeContentOfElementUsingEnrichMediator");
+    }
+
+    //This test is to verify if payload can be modified by removing content of an element using xslt mediator.
+    @Test(description = "1.6.4.3")
+    public void removeContentOfElementUsingXsltMediator() throws IOException, XMLStreamException {
+        String url = getProxyServiceURLHttp("1_6_4_3_Proxy_removeElementUsingXsltMediator");
+        String testCaseId = "1.6.4.3";
+        String request =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                        + "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                        + "   <soap:Header />\n"
+                        + "   <soap:Body>\n"
+                        + "      <Employee>\n"
+                        + "         <firstName>Isuru</firstName>\n"
+                        + "         <lastName>Uyanage</lastName>\n"
+                        + "         <city>Colombo</city>\n"
+                        + "         <preferredName>Isuru</preferredName>\n"
+                        + "      </Employee>\n"
+                        + "   </soap:Body>\n"
+                        + "</soap:Envelope>";
+
+        String expectedResponse =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                        + "<soap:Envelope xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n"
+                        + "   <soap:Header />\n"
+                        + "   <soap:Body>\n"
+                        + "      <Employee>\n"
+                        + "         <firstName>Isuru</firstName>\n"
+                        + "         <lastName>Uyanage</lastName>\n"
+                        + "         <city>Colombo</city>\n"
+                        + "      </Employee>\n"
+                        + "   </soap:Body>\n"
+                        + "</soap:Envelope>";
+
+        HTTPUtils.invokeSoapActionAndAssert(url, request, testCaseId, expectedResponse, 200,
+                "urn:mediate", "removeContentOfElementUsingXsltMediator");
     }
 
     @AfterClass(description = "Server Cleanup", alwaysRun = true)
