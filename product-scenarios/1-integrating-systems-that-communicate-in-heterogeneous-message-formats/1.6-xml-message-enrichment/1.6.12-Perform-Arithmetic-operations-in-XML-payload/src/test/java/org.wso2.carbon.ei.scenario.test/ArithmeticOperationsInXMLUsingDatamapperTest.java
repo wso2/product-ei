345f3f5b095507591c3393e55a16b60febbcbc37
Isuru Uyanage <isuruuy@gmail.com>
2019-02-06 15:00:52 +0530

    add test cases for 1.6.12.2 and 1.6.12.3

--- product-scenarios/1-integrating-systems-that-communicate-in-heterogeneous-message-formats/1.6-xml-message-enrichment/1.6.12-Perform-Arithmetic-operations-in-XML-payload/src/test/java/org.wso2.carbon.ei.scenario.test/ArithmeticOperationsInXMLUsingDatamapperTest.java
+++ product-scenarios/1-integrating-systems-that-communicate-in-heterogeneous-message-formats/1.6-xml-message-enrichment/1.6.12-Perform-Arithmetic-operations-in-XML-payload/src/test/java/org.wso2.carbon.ei.scenario.test/ArithmeticOperationsInXMLUsingDatamapperTest.java
@@ -62,6 +62,61 @@ public class ArithmeticOperationsInXMLUsingDatamapperTest extends ScenarioTestBa
 
     }
 
+    /**
+     * This test is to verify if substract operation can be performed with given xml payload using
+     *  datamapper mediator.
+     */
+    @Test(description = "1.6.12.2")
+    public void performSubstractOperationUsingDatamapper() throws IOException, XMLStreamException {
+        String apiName = "1_6_12_2_API_performSubstractOperationUsingDatamapper";
+        String testCaseId = "1.6.12.2";
+        String apiInvocationUrl = getApiInvocationURLHttp(apiName);
+
+        String request =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                        + "<SubstractNumbers>\n"
+                        + "    <num1>51</num1>\n"
+                        + "    <num2>6</num2>\n"
+                        + "</SubstractNumbers>";
+
+        String expectedResponse =
+                "<ResultSubstract>\n"
+                        + "    <result>45.0</result>\n"
+                        + "</ResultSubstract>";
+
+        HTTPUtils.invokePoxEndpointAndAssert(apiInvocationUrl,request, HTTPConstants.MEDIA_TYPE_APPLICATION_XML,
+                testCaseId, expectedResponse, 200, "performSubstractOperationUsingDatamapper");
+
+    }
+
+    /**
+     * This test is to verify if multiplication operation can be performed with given xml payload using
+     *  datamapper mediator.
+     */
+    @Test(description = "1.6.12.3")
+    public void performMultiplicationOperationUsingDatamapper() throws IOException, XMLStreamException {
+        String apiName = "1_6_12_3_API_performMultiplicationOperationUsingDatamapper";
+        String testCaseId = "1.6.12.3";
+        String apiInvocationUrl = getApiInvocationURLHttp(apiName);
+
+        String request =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                        + "<MultiplyNumbers>\n"
+                        + "    <num1>5</num1>\n"
+                        + "    <num2>6</num2>\n"
+                        + "</MultiplyNumbers>";
+
+        String expectedResponse =
+                "<ResultMultiply>\n"
+                        + "    <result>30.0</result>\n"
+                        + "</ResultMultiply>";
+
+        HTTPUtils.invokePoxEndpointAndAssert(apiInvocationUrl,request, HTTPConstants.MEDIA_TYPE_APPLICATION_XML,
+                testCaseId, expectedResponse, 200, "performMultiplicationOperationUsingDatamapper");
+
+    }
+
+
     @AfterClass(description = "Server Cleanup", alwaysRun = true)
     public void cleanup() throws Exception {
         super.cleanup();
