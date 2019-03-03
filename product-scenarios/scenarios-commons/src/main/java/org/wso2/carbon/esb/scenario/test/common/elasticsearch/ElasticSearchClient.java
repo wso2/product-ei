d14da97dd5cb054356820bd59242c360f777568d
sdkottegoda <sdkottegoda@gmail.com>
2019-03-03 17:34:48 +0530

    Encode query param

--- product-scenarios/scenarios-commons/src/main/java/org/wso2/carbon/esb/scenario/test/common/elasticsearch/ElasticSearchClient.java
+++ product-scenarios/scenarios-commons/src/main/java/org/wso2/carbon/esb/scenario/test/common/elasticsearch/ElasticSearchClient.java
@@ -53,15 +53,17 @@ public class ElasticSearchClient {
      *
      * @param logSnippet log snippet need to match
      * @return found log hits
-     * @throws IOException
+     * @throws IOException if an error occurs during the invocation
      */
-    public JSONObject searchCarbonLogs(String logSnippet) throws IOException {
+    private JSONObject searchCarbonLogs(String logSnippet) throws IOException {
 
-        String searchURL = hostName + "/" + deploymentStackID + "-carbonlogs*/_search?q=\"" + logSnippet + "\"";
-        searchURL = URLEncoder.encode(searchURL, "UTF-8");
-        log.info("searchURL:" + searchURL);
+        String baseUrl = "http://" + hostName + "/" + deploymentStackID + "-carbonlogs*/_search?q=";
+        String queryParam = URLEncoder.encode("\"" + logSnippet + "\"", "UTF-8");
+        String searchUrl = baseUrl + queryParam;
+
+        log.info("searchURL:" + searchUrl);
         RESTClient restClient = new RESTClient();
-        HttpResponse response = restClient.doGet(searchURL);
+        HttpResponse response = restClient.doGet(searchUrl);
 
         JSONObject resp = new JSONObject(HTTPUtils.getResponsePayload(response));
         log.info("Response" + resp.toString());
