2c93896c0ff30ec95b1b8e452a5e6ab8434b68d7
sameerag <sameera910409@gmail.com>
2019-10-25 18:15:06 +0530

    Fix nhttp access log test cases

--- integration/mediation-tests/tests-patches/src/test/java/org/wso2/carbon/esb/passthru/transport/test/HttpAccessLogTestCase.java
+++ integration/mediation-tests/tests-patches/src/test/java/org/wso2/carbon/esb/passthru/transport/test/HttpAccessLogTestCase.java
@@ -55,7 +55,7 @@ public class HttpAccessLogTestCase extends ESBIntegrationTest {
         super.init();
         serverConfigurationManager = new ServerConfigurationManager(new AutomationContext("ESB", TestUserMode.SUPER_TENANT_ADMIN));
         String nhttpFile = FrameworkPathUtil.getSystemResourceLocation() + "artifacts" + separator + "ESB" + separator +
-                "passthru" + separator + "transport" + separator + "httpaccesslogs" + separator + "nhttp.properties";
+                "passthru" + separator + "transport" + separator + "httpaccesslogs" + separator + "access-log.properties";
 
         File srcFile = new File(nhttpFile);
         String carbonHome = System.getProperty(ServerConstants.CARBON_HOME);
