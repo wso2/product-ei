9b2a4e57c04a4cc0494335607f96d6c4ff0d9d8a
sameerag <sameera910409@gmail.com>
2019-11-25 12:04:07 +0530

    Fix suggested changes

--- components/org.wso2.ei.admin.ui/src/main/resources/web/server-admin/js/serveradmin.js
+++ components/org.wso2.ei.admin.ui/src/main/resources/web/server-admin/js/serveradmin.js
@@ -1,18 +1,20 @@
 /*
- ~ Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- ~
- ~ Licensed under the Apache License, Version 2.0 (the "License");
- ~ you may not use this file except in compliance with the License.
- ~ You may obtain a copy of the License at
- ~
- ~      http://www.apache.org/licenses/LICENSE-2.0
- ~
- ~ Unless required by applicable law or agreed to in writing, software
- ~ distributed under the License is distributed on an "AS IS" BASIS,
- ~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- ~ See the License for the specific language governing permissions and
- ~ limitations under the License.
- */
+*  Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 function shutdownServerGracefully() {
     jQuery.noConflict();
     CARBON.showConfirmationDialog(jsi18n["graceful.shutdown.verify"],shutdownServerGracefullyCallback,null);
@@ -25,10 +27,6 @@ function shutdownServerGracefullyCallback() {
         {
             "action":"shutdownGracefully"
         },function (responseText, status, XMLHttpRequest) {
-            /*        if (jQuery.trim(responseText) != '') {
-                CARBON.showWarningDialog(responseText);
-                return;
-            }*/
             if (status != "success") {
                 CARBON.showErrorDialog(jsi18n["graceful.shutdown.error"]);
             } else {
